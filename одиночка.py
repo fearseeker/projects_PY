7b7e27f5ca13ac3416f1562dfa7168e7f7f4fd2f
Litva Alex <cah9ivin@gmail.com>
2025-09-11 22:50:45 +0300

    сделал паттерн одиночка через метакласс

--- одиночка.py
+++ одиночка.py
@@ -1,11 +1,20 @@
-class Single:
-    #проверяю есть ли уже объект
-    instance = None
+class SingleMeta(type):
+    _instance = {}
 
-    def __new__(cls):
-        if Single.instance is None:
-            Single.instance = super().__new__(cls)
-        return Single.instance
-S1 = Single()
-S2 = Single()
-print(S1 is S2)
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instance:
+            cls._instance[cls] = super().__call__(*args, **kwargs)
+        return cls._instance[cls]
+
+
+class A(metaclass=SingleMeta):
+    def __init__(self, a):
+        self.a = a
+
+
+a1 = A(2)
+a2 = A(5)
+print(a1.a, a2.a)
+a2.a = 5
+print(a1 is a2)
+print(a1.a, a2.a)
